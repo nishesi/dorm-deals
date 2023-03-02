b54864207ef2ac968a939516cc070cc7a55ff224
Ainur Sabitov <ainursabitovak@mail.ru>
2023-03-02 19:57:10 +0300

    add ExceptionDto and ShopsApi

---
+++ src/main/java/ru/itis/master/party/dormdeals/dto/ExceptionDto.java
@@ -0,0 +1,19 @@
+package ru.itis.master.party.dormdeals.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Schema(description = "Сведения об ошибке")
+public class ExceptionDto {
+    @Schema(description = "Текст ошибки", example = "Магазин не найден")
+    private String message;
+    @Schema(description = "HTTP-код ошибки", example = "404")
+    private int status;
+}
