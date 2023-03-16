77bc3ad89ef332307c54230fc139745b473353ac
cbman1999 <38330163+cbman1999@users.noreply.github.com>
2023-03-16 18:02:05 +0300

    some fixes 2.0 and extract two methods in utils

---
+++ src/main/java/ru/itis/master/party/dormdeals/utils/OwnerChecker.java
@@ -0,0 +1,25 @@
+package ru.itis.master.party.dormdeals.utils;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import ru.itis.master.party.dormdeals.exceptions.NotAllowedException;
+import ru.itis.master.party.dormdeals.exceptions.NotFoundException;
+import ru.itis.master.party.dormdeals.models.User;
+import ru.itis.master.party.dormdeals.repositories.UserRepository;
+
+import java.util.Objects;
+
+
+@Component
+public class OwnerChecker {
+    public void checkOwnerShop(Long ownerShopId, User thisUser) {
+        if (!Objects.equals(thisUser.getId(), ownerShopId)) {
+            throw new NotAllowedException("Вы не являетесь владельцем данного магазина.");
+        }
+    }
+    public User initThisUser(UserRepository userRepository) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.getByEmail(authentication.getName()).orElseThrow(() -> new NotFoundException("Пользователь не найден"));
+    }
+}
