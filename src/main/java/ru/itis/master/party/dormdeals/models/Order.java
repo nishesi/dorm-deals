83eed480244c6e7865f9c076002607f33bf02883
Ainur Sabitov <ainursabitovak@mail.ru>
2023-03-16 21:30:14 +0300

    add order model

---
+++ src/main/java/ru/itis/master/party/dormdeals/models/Order.java
@@ -0,0 +1,46 @@
+package ru.itis.master.party.dormdeals.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Table(name = "orders")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+    public enum State {
+        IN_PROCESSING,
+        CONFIRMED,
+        IN_DELIVERY,
+        DELIVERED
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy hh-mm")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date orderTime;
+
+    private String userComment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shop_id")
+    private Shop shop;
+
+    @Enumerated(value = EnumType.STRING)
+    private State state;
+}
