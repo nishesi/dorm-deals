bb89d27b1667a112d5f3fa4c5130cd26869bf6e8
Ainur Sabitov <ainursabitovak@mail.ru>
2023-03-16 21:51:03 +0300

    add order's product dto

--- src/main/java/ru/itis/master/party/dormdeals/models/OrderProduct.java
+++ src/main/java/ru/itis/master/party/dormdeals/models/OrderProduct.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class OrderProducts {
+public class OrderProduct {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
