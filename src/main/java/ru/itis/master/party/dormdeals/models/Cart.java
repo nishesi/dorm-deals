d4493f4e52f050bdd1e6c998e0e204b1beb695fc
cbman1999 <38330163+cbman1999@users.noreply.github.com>
2023-03-17 22:24:46 +0300

    add links with user and product in cart

--- src/main/java/ru/itis/master/party/dormdeals/models/Cart.java
+++ src/main/java/ru/itis/master/party/dormdeals/models/Cart.java
@@ -2,16 +2,25 @@ package ru.itis.master.party.dormdeals.models;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    //TODO: навесить нужны аннотации, чтобы создать связи
-    private Long userId;
-    private Long productId;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne()
+    @JoinColumn(name = "product_id")
+    private Product product;
     private Long count;
-    private Long sumPricesOfProducts;
 }
