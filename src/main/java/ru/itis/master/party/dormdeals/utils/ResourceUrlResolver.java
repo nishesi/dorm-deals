10417edd95de56e8aea6ce0b31c95ef3ef6ee9b7
Nurislam Zaripov <znurislama56@gmail.com>
2023-03-18 21:16:26 +0300

    remove exception

--- src/main/java/ru/itis/master/party/dormdeals/utils/ResourceUrlResolver.java
+++ src/main/java/ru/itis/master/party/dormdeals/utils/ResourceUrlResolver.java
@@ -1,12 +1,12 @@
 package ru.itis.master.party.dormdeals.utils;
 
-import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ResourceUrlResolver {
 
     public String resolveUrl(String id, ResourceType type) {
-        throw new NotImplementedException();
+        //todo make real realization
+        return "/resource/test/url/12451";
     }
 }
