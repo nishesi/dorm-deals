9651eb9c74add6a3bf0cc48f0e29eb245f71c27d
Nurislam Zaripov <znurislama56@gmail.com>
2023-07-18 11:42:31 +0300

    improve service method

--- frontend/src/API/SearchProductService.js
+++ frontend/src/API/SearchProductService.js
@@ -2,8 +2,29 @@ import axios from "axios";
 
 export default class SearchProductService {
     static async getByCriteria(categories, nameQueries, shopIds, pageInd) {
+        let params = {
+            pageInd: pageInd !== undefined ? pageInd : 0
+        }
+        if (categories !== undefined && categories.length !== 0) {
+            params = {
+                categories : categories.join(",")
+            }
+        }
+        if (nameQueries !== undefined && nameQueries.length !== 0) {
+            params = {
+                ...params,
+                "name-query" : nameQueries.join(",")
+            }
+        }
+        if (shopIds !== undefined && shopIds.length !== 0) {
+            params = {
+                ...params,
+                "shop-id": shopIds.join(",")
+            }
+        }
+
         let requestConfig = {
-            params: [...categories, ...nameQueries, ...shopIds, pageInd !== undefined ? pageInd : 0]
+            params: params
         }
         const response = await axios.get("http://localhost/app/search/products", requestConfig);
         return response.data;
